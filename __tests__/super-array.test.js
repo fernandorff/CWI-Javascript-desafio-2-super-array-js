6c78da9d63dcddc23e07cf9a0900cb32180c64ba
gus <gustavo.rodrigues@cwi.com.br>
2021-02-05 16:51:41 -0300

    adicionando exercicio

---
+++ __tests__/super-array.test.js
@@ -0,0 +1,8 @@
+import instrutores from './instrutores.json'
+import { SuperArray } from '../src/super-array'
+
+describe('Exemplo de testes', () => {
+  it('Valor importado deve ser true', () => {
+    expect(exemplo).toBeTruthy()
+  })
+})
---
+++ src/super-array.js
@@ -0,0 +1,65 @@
+export const SuperArray = (...itens) => {
+
+  const array = {
+    /**
+     * Propriedade para acessar os itens
+     */
+
+    itens: [...itens],
+  }
+
+  /**
+   * Adicionar um novo item ao final dos items
+   */
+
+  array.push = item => {
+    return null
+  }
+
+  /**
+   * Itera sobre cada um dos elementos do SuperArray enviando o item e o index
+   * como segundo parametro
+   */
+
+  array.forEach = callback => {
+    return null
+  }
+
+  /**
+   * Retorna um novo SuperArray com os itens mapeados
+   */
+
+  array.map = callback => {
+    return null
+  }
+
+
+  /**
+   * Retorna um SuperArray novo com os itens filtrados
+   */
+
+  array.filter = callback => {
+    return null
+  }
+
+
+  /**
+   * Retorna o primeiro elemento do SuperArray que satisfazer o callback recebido
+   * se não encontrar, deve retornar undefined
+   */
+
+  array.find = callback => {
+    return null
+  }
+
+  /**
+   * Reduz o SuperArray em um único valor
+   */
+
+
+  array.reduce = (callback, valorInicial) => {
+    return null
+  }
+
+  return array
+}
